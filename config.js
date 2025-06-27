cb73e17d546a28c2209c490db49ef899713aa281
findus-jdk <findus.jdk@gmail.com>
2025-06-27 11:44:20 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,14 +1,19 @@
+// config.js
 const companyConfig = {
     companyName: "DRASHTI HAIR ART",
     slogan: "Innovating for a better tomorrow",
-    logoUrl: "logo.jpeg",
+    logoUrl: "logo.jpeg",  // Fixed path to match your HTML
+    backgroundImage: "./images/background.png",  // Fixed path to match your CSS
     contact: {
-        phone: "+918490046650",
+        phone: "+919033311330",
+        address: " Sai villa complex shop no 9 fulsar chitra ,bhavnagar, Gujarat 364004"
     },
     socialMedia: {
-        whatsapp: "https://wa.me/918490046650",
-        instagram: "https://www.instagram.com/vandanlining1234?igsh=MWl5YjM1eGhla2tvcQ==",
-        googleReviews: "https://www.google.com/search?sca_esv=c00251c694cd13aa&sxsrf=AE3TifMZuYciS-4X1MUEpevpCuJhARPNSA:1750245339926&si=AMgyJEtREmoPL4P1I5IDCfuA8gybfVI2d5Uj7QMwYCZHKDZ-EwiG8AMeEMcVmjH6rYC0EqEj5U9-O1SDUv0zu5MzaMlCZmlm3uUTTPTjN0Ld1xCmzMDeYwWuOCC79eEishhmipOqSsQGV0xoOaeoi20bn3rGBReG7w%3D%3D&q=Vandan+Learning+Works+Reviews&sa=X&ved=2ahUKEwiF_IX-6_qNAxXayzgGHQzgCLYQ0bkNegQIGxAD&biw=1600&bih=792&dpr=1.8#lrd=0x395f5a87119e43f3:0x39f779eed0000000,3,,,,",
-        map: "https://www.google.com/maps/dir//Kesarbay+Magjida,+Navapara+Rd,+Bhavnagar,+Gujarat+364001/@21.7736056,72.0671765,12z/data=!4m8!4m7!1m0!1m5!1m1!1s0x395f5a87119e43f3:0x39f779eed0000000!2m2!1d72.1495781!2d21.773626?entry=ttu&g_ep=EgoyMDI1MDYxNS4wIKXMDSoASAFQAw%3D%3D"
+        whatsapp: "https://wa.me/916355604903",
+        // instagram: "https://instagram.com/jdkgroupofficial",
+        // linkedin: "https://linkedin.com/company/jdk-group",
+        // website: "https://jdkgroup.in/",
+        googleReviews:"https://www.google.com/maps/dir/21.7577997,72.1452392/P3WR%2BHRQ+Drashti+Hair+Art,+Swaminarayan+Temple+Rd,+Indiranagar,+Chitra,+Bhavnagar,+Gujarat+364004/@21.7554435,72.0978334,14z/data=!3m1!4b1!4m17!1m7!3m6!1s0x395f51937bf36e17:0x4a71e2e7f70376b4!2sDrashti+Hair+Art!8m2!3d21.7464541!4d72.0921168!16s%2Fg%2F11r702b67l!4m8!1m1!4e1!1m5!1m1!1s0x395f51937bf36e17:0x4a71e2e7f70376b4!2m2!1d72.0921206!2d21.7464545?entry=ttu&g_ep=EgoyMDI1MDYyMy4yIKXMDSoASAFQAw%3D%3D",
+        map: "https://www.google.com/maps/dir/21.7577997,72.1452392/P3WR%2BHRQ+Drashti+Hair+Art,+Swaminarayan+Temple+Rd,+Indiranagar,+Chitra,+Bhavnagar,+Gujarat+364004/@21.7554435,72.0978334,14z/data=!3m1!4b1!4m17!1m7!3m6!1s0x395f51937bf36e17:0x4a71e2e7f70376b4!2sDrashti+Hair+Art!8m2!3d21.7464541!4d72.0921168!16s%2Fg%2F11r702b67l!4m8!1m1!4e1!1m5!1m1!1s0x395f51937bf36e17:0x4a71e2e7f70376b4!2m2!1d72.0921206!2d21.7464545?entry=ttu&g_ep=EgoyMDI1MDYyMy4yIKXMDSoASAFQAw%3D%3D"
     }
 };
