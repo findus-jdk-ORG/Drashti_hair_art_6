1d091cc37d08815fe8c825d497eba2cea88d3684
findus-jdk <findus.jdk@gmail.com>
2025-06-27 17:51:32 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 // config.js
 const companyConfig = {
     companyName: "DRASHTI HAIR ART",
-    slogan: "Innovating for a better tomorrow",
+    slogan: "Where Beauty Meets Perfection",
     logoUrl: "logo.jpeg",  // Fixed path to match your HTML
     backgroundImage: "./images/background.png",  // Fixed path to match your CSS
     contact: {
@@ -10,7 +10,7 @@ const companyConfig = {
     },
     socialMedia: {
         whatsapp: "https://wa.me/919033311330",
-         instagram: "https://www.instagram.com/drashti_hairsaloon?igsh=ZW5neDRhbDk3ZmQ1",
+        instagram: "https://www.instagram.com/drashti_hairsaloon/?igsh=ZW5neDRhbDk3ZmQ1#",
         // linkedin: "https://linkedin.com/company/jdk-group",
         // website: "https://jdkgroup.in/",
         googleReviews:"https://www.google.com/maps/dir/21.7577997,72.1452392/P3WR%2BHRQ+Drashti+Hair+Art,+Swaminarayan+Temple+Rd,+Indiranagar,+Chitra,+Bhavnagar,+Gujarat+364004/@21.7554435,72.0978334,14z/data=!3m1!4b1!4m17!1m7!3m6!1s0x395f51937bf36e17:0x4a71e2e7f70376b4!2sDrashti+Hair+Art!8m2!3d21.7464541!4d72.0921168!16s%2Fg%2F11r702b67l!4m8!1m1!4e1!1m5!1m1!1s0x395f51937bf36e17:0x4a71e2e7f70376b4!2m2!1d72.0921206!2d21.7464545?entry=ttu&g_ep=EgoyMDI1MDYyMy4yIKXMDSoASAFQAw%3D%3D",
